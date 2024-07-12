8911a823ca855031ba852ba531497905041be33e
SaiLokeshReddy7 <sailokesh9110@gmail.com>
2024-07-12 09:55:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip()]
+
+        # Remove '-e .' if present, because we should handle it outside the requirements list
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Gaddam Sai Lokesh Reddy',
+    author_email='sailokes.cse.2022@outlook.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
